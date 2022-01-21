9f27b8c72da78dbb59472d94551d5860c2d59661
Bellie Joe M. Jandusay <jandusayjoe14@gmail.com>
2022-01-21 13:28:38 +0800

    last day commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 
+function TasksList() {
+  return (
+    <ul>
+      <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
+      <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
+      <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
+    </ul>
+  );
+}
+
 
 // my first component
 function App () {
@@ -16,6 +26,9 @@ function App () {
 				<h1>My Amazing To Do List App</h1>
 
 				<p>The most simple and amazing todo-list React app.</p>
+
+
+		  		<TasksList/>
 
 			</header>
 
